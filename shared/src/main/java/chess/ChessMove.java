90bbbce9a19f5e789fa552a5f13a627a45a7732a
Jacob <jmemmott13@gmail.com>
2024-09-06 19:01:45 -0600

    added equals and hashset overrides, not sure what they're supposed to do yet

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents moving a chess piece on a chessboard
  * <p>
@@ -22,8 +24,9 @@ public class ChessMove {
     /*
      * toString override method
      */
+    @Override
     public String toString() {
-        return getStartPosition().toString() + ":" + getEndPosition().toString();
+        return /*getStartPosition().toString() + ":" + */ getEndPosition().toString();
     }
 
     /**
@@ -48,5 +51,18 @@ public class ChessMove {
      */
     public ChessPiece.PieceType getPromotionPiece() {
         throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessMove chessMove = (ChessMove) o;
+        return Objects.equals(startPosition, chessMove.startPosition) && Objects.equals(endPosition, chessMove.endPosition) && promotionPiece == chessMove.promotionPiece;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(startPosition, endPosition, promotionPiece);
     }
 }
--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents a single square position on a chess board
  * <p>
@@ -17,6 +19,7 @@ public class ChessPosition {
     }
     /* override toString
     */
+    @Override
     public String toString() {
         String letter = "";
         switch(getColumn()){
@@ -64,5 +67,18 @@ public class ChessPosition {
      */
     public int getColumn() {
         return col;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessPosition that = (ChessPosition) o;
+        return row == that.row && col == that.col;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, col);
     }
 }
