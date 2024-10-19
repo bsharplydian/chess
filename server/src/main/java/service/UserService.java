85ce33f763111801b9634912cd4b67121a590c96
Jacob <jmemmott13@gmail.com>
2024-10-18 19:15:59 -0600

    refactor(register logic)

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -14,24 +14,32 @@ public class UserService {
     public UserService(DataAccess dataAccess) {
         this.dataAccess = dataAccess;
     }
+
     public RegisterResponse register(RegisterRequest request) throws DataAccessException {
         RegisterResponse response;
-        if(request.username() == null || request.email() == null || request.password() == null)
+        if(invalidRegisterInput(request))
             response = new RegisterResponse(null, null, "Error: bad request");
-        else if(dataAccess.getUser(request.username()) == null) { // no match -> register given user
+        else if(dataAccess.getUser(request.username()) != null) {
+            response = new RegisterResponse(null, null, "Error: already taken");
+        }
+
+        else { // no match -> register given user
             dataAccess.createUser(new UserData(request.username(), request.password(), request.email()));
+
             String token = UUID.randomUUID().toString();
             AuthData auth = new AuthData(request.username(), token);
             dataAccess.createAuth(auth);
             response = new RegisterResponse(request.username(), token, null);
         }
-        else {
-            response = new RegisterResponse(null, null, "Error: already taken");
-        }
+
+
         
         return response;
     }
-    public Boolean validateAuth(AuthData auth) throws DataAccessException {
+    private Boolean invalidRegisterInput(RegisterRequest request) {
+        return request.username() == null || request.email() == null || request.password() == null;
+    }
+    private Boolean validateAuth(AuthData auth) throws DataAccessException {
         return true;
     }
     public void clear() {
