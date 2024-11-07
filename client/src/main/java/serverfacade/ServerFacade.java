b27ebdf2200a182b5a8d8ac77036c70025e8d88c
Jacob <jmemmott13@gmail.com>
2024-11-06 18:38:13 -0700

    create(initial framework phase 5)

---
+++ client/src/main/java/serverfacade/ServerFacade.java
@@ -0,0 +1,6 @@
+package serverfacade;
+
+public class ServerFacade {
+
+
+}
---
+++ client/src/main/java/ui/UI.java
@@ -0,0 +1,10 @@
+package ui;
+
+import chess.ChessBoard;
+
+public class UI {
+
+    public void displayBoard(ChessBoard board) {
+
+    }
+}
