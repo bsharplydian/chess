46e32fbb8ed952af8be88646b1d93f5867a7d18e
Jacob <jmemmott13@gmail.com>
2024-11-06 20:45:18 -0700

    add(tic tac toe logic)

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,11 @@
 import chess.*;
+import ui.UI;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+        UI ui = new UI();
+        ui.displayBoard(new ChessBoard());
     }
 }
