3a11989df65dbcba476fadf8b053a2db8eb0823f
Jacob <jmemmott13@gmail.com>
2024-11-13 18:30:29 -0700

    changed port to 0 for autograder

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -15,7 +15,7 @@ public class ServerFacadeTests {
     @BeforeAll
     public static void init() throws Exception {
         server = new Server();
-        var port = server.run(8080);
+        var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
 
         serverFacade = new ServerFacade("http://localhost:" + port);
